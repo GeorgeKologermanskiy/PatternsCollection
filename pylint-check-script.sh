e8ec9eca97e694ea999edba692de28b1ab6e1cec
GeorgeKologermanskiy <georgekolog@gmail.com>
2021-11-01 03:16:07 +0300

    add linter

---
+++ pylint-check-script.sh
@@ -0,0 +1,7 @@
+PYTHON_FILES=$(find . -name \*.py -print)
+
+RES=$(pylint $PYTHON_FILES | tail -2)
+echo $RES
+
+SCORE=$(echo $RES | awk '{print $7}' | awk -F/ '{print $1}')
+echo $SCORE | awk --source '{exit ($0 < 9.00)}'
